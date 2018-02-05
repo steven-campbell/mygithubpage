82a78a89d7c5fed18b7da00e45f0797f06e80cd1
Steven Campbell <user@example.co>
2018-02-04 21:17:55 -0500

    add tree styling

--- saa/index.js
+++ saa/index.js
@@ -1,9 +1,14 @@
-function draw(startX, startY, len, angle, delta) {
+function draw(startX, startY, len, angle, delta, branchWidth) {
   ctx.beginPath();
   ctx.save();
   
   ctx.translate(startX, startY);
   ctx.rotate(angle * Math.PI/180);
+
+  ctx.shadowBlur = 15;
+  ctx.shadowColor = "rgba(0,0,0,0.7)";
+  ctx.lineWidth = branchWidth;
+
   ctx.moveTo(0, 0);
   ctx.lineTo(0, -len);
   ctx.stroke();
@@ -13,9 +18,12 @@ function draw(startX, startY, len, angle, delta) {
     return;
   }
   
-  draw(0, -len, len*0.8, -15 - delta/2, -delta);
-  draw(0, -len, len*0.8, 15 + delta/2, delta);
+  draw(0, -len, len*0.8, -15 - delta/2, -delta, branchWidth*0.8);
+  draw(0, -len, len*0.8, 15 + delta/2, delta, branchWidth*0.8);
   
+
+
+
   ctx.restore();
 }
 
@@ -24,12 +32,12 @@ function updateTree() {
 	angle = 0;
 	delta = document.getElementById("delta").value;
 	ctx.clearRect(0, 0, 700, 600);
-	draw(350, 600, length, angle, delta);
+	draw(350, 600, length, angle, delta, 10);
 }
 
 ctx = document.getElementById("tree").getContext("2d");
 
-draw(350, 600, 120, 0, 0);
+draw(350, 600, 120, 0, 0, 10);
 
 document.getElementById("length").defaultValue = "120";
 document.getElementById("delta").defaultValue = "0";
